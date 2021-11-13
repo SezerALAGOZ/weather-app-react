80e65a05b2b1c7b91816cb8c0edf39600718dbc1
Sezer Alagoz <sezeralagoz8@gmail.com>
2021-11-14 01:20:19 +0300

    env deleted and added gitignore

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { WeatherProvider } from "./Context/WeatherContext";
+import Deneme from "./components/Deneme";
 
 function App() {
   return (
     <WeatherProvider>
       <div className="App">
+        <Deneme />
       </div>
     </WeatherProvider>
   );
--- src/components/Deneme.js
+++ src/components/Deneme.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import data from './Context/data.json';
+import data from '../Context/data.json';
+import {useEffect, useState} from 'react';
 
 function Deneme() {
-    console.log(data);
+    
+    const [hava, setHava] = useState({});
+    const lon = data.find(item => item.name === 'ADANA').long;
+    const lat = data.find(item => item.name === 'ADANA').lat;
+    const API_Key = process.env.REACT_APP_API_KEY;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${API_Key}`;
+
+    useEffect(() => {
+        fetch(apiUrl)
+        .then(res => res.json())
+        .then(data => setHava(data))
+    }, [apiUrl])
+    
     return (
         <div>
-            
+            {JSON.stringify(hava)}
         </div>
     )
 }
